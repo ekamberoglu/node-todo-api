90288b17c51dc2babf80c52b41a790f5d28bf285
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2017-04-13 14:24:24 -0700

    Setting user and password

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://<dbuser>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
+  mlab: 'mongodb://kayroc:ekamber0glu@ds161210.mlab.com:61210/todoapp'
 };
 mongoose.connect( db.localhost || db.mlab);
 
