d1b2bf5e1a7e5a384a12ec1314d82cf91661531f
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2017-04-13 14:45:41 -0700

    Comment out localhost for mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,7 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://kayroc:ekamber0glu@ds161210.mlab.com:61210/todoapp'
 };
-mongoose.connect( db.localhost || db.mlab);
+//mongoose.connect( db.localhost || db.mlab);
+mongoose.connect(db.mlab);
 
 module.exports = {mongoose};
