f65fb44c9ea81d9cbcad92d484b65788325a207b
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2017-04-13 22:05:41 -0700

    Add PATCH /todos/:id rute

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,13 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+// mlab: 'mongodb://<dbuser>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
+
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://<dbuser>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
+  mlab: 'mongodb://kayroc:ekamber0glu@ds161210.mlab.com:61210/todoapp'
 };
-mongoose.connect( db.localhost || db.mlab);
-// mongoose.connect(db.mlab);
+//mongoose.connect( db.localhost || db.mlab);
+mongoose.connect(db.mlab);
 
 module.exports = {mongoose};
