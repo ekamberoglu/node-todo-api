00a6386ea2985e71c00d71e43f629a67476b48eb
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2017-04-14 01:50:26 -0700

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,17 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
-
-// mlab: 'mongodb://<dbuser>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
-console.log('env *****', env);
-
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else {
-  process.env.MONGODB_URI = 'mongodb://kayroc:ekamber0glu@ds161210.mlab.com:61210/todoapp';
-}
+require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
