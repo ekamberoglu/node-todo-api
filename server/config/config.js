10e57bf62cb204b715d662b1264501a4b8dfb7a4
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2017-04-14 01:57:39 -0700

    Refactoring

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,5 @@
 var env = process.env.NODE_ENV || 'development';
 
-// mlab: 'mongodb://<dbuser>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
-
 if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
@@ -9,5 +7,5 @@ if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else {
-  process.env.MONGODB_URI = 'mongodb://kayroc:ekamber0glu@ds161210.mlab.com:61210/todoapp';
+  process.env.MONGODB_URI = 'mongodb://kayroc:<dbpassword>@ds161210.mlab.com:61210/todoapp';
 }
