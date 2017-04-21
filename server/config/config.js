7534777b96ed4667547becb271479341d0159d77
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2017-04-20 23:09:56 -0700

    Add comment

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,15 @@
 var env = process.env.NODE_ENV || 'development';
 
-// Get development, test or production values
-var config = require('./config.json');
-var envConfig = config[env];
+// set JWT_SECRET value and MONGODB_URI = 'mongodb://<dbusername>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
+// from heroku config:set 
+// ie. heroku config:set JWT_SECRET=fsdafa4132414
+// heroku config:set MONGODB_URI='mongodb://<dbusername>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
 
-Object.keys(envConfig).forEach((key) => {
-  process.env[key] = envConfig[key];
-});
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
