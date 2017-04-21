7d8fb3cda907643ff905a94ef91414b132856080
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2017-04-21 00:05:50 -0700

    Modify comment

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 var env = process.env.NODE_ENV || 'development';
 
-// set JWT_SECRET value and MONGODB_URI = 'mongodb://<dbusername>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
+// set JWT_SECRET value and MONGODB_URI
 // from heroku config:set 
 // ie. heroku config:set JWT_SECRET=fsdafa4132414
 // heroku config:set MONGODB_URI='mongodb://<dbusername>:<dbpassword>@ds161210.mlab.com:61210/todoapp'
